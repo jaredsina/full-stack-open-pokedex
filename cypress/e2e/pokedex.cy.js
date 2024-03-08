e7e3c4ac924e2bac75721dcc4b9022e7c768ff11
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-03-08 11:47:22 -0500

    fix: port number for cypress testing

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -3,7 +3,7 @@
 
 describe('Pokedex', () => {
   it('front page can be opened', () => {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:9000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
   })
