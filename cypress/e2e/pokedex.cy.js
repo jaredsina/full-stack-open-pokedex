23edab792f5882b5abae6489bad3c2c96768315c
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-03-08 11:54:31 -0500

    add e2e testing for pokemon page

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -7,4 +7,12 @@ describe('Pokedex', () => {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
   })
+  it('can navigate to a pokemon page', () => {
+    cy.visit('http://localhost:9000')
+    cy.contains('ivysaur')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
+  })
 })
+
